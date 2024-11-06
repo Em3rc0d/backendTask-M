e69fb51dfc771b36083bb0e54aedad8984500f23
em3rc0d <farid.merino1673@gmail.com>
2024-11-05 20:28:25 -0500

    v2.0

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
-const app = require('./app'); // Importa la aplicación Express
+// const app = require('./app'); // Importa la aplicación Express
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Servidor corriendo en el puerto ${PORT}`);
+// });

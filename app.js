2508cad5381ed393625a51021c6728600eedc375
farid <farid.merino1673@gmail.com>
2024-11-11 13:11:43 -0500

    gg

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const { router: authRouter, authenticateJWT } = require('./api/auth');
-const tasksRoute = require('./api/tasks');
-require('dotenv').config(); // Para cargar las variables de entorno
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { router: authRouter, authenticateJWT } = require("./api/auth");
+const tasksRoute = require("./api/tasks");
+require("dotenv").config(); // Para cargar las variables de entorno
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://backend-task-m.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 // Conectar a la base de datos
@@ -16,41 +22,41 @@ const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 let isConnected = false; // Variable para almacenar el estado de la conexión
 
 async function connectToDatabase() {
-    if (isConnected) {
-        console.log("Conexión a MongoDB ya está establecida.");
-        return;
-    }
-    try {
-        const db = await mongoose.connect(mongoUri, { 
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 20000 // Tiempo máximo de espera de 5 segundos
-        });
-        isConnected = db.connections[0].readyState === 1; // 1 significa conectado
-        console.log("Conectado a MongoDB");
-    } catch (error) {
-        console.error("Error de conexión a MongoDB:", error);
-    }
+  if (isConnected) {
+    console.log("Conexión a MongoDB ya está establecida.");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000, // Tiempo máximo de espera de 5 segundos
+    });
+    isConnected = db.connections[0].readyState === 1; // 1 significa conectado
+    console.log("Conectado a MongoDB");
+  } catch (error) {
+    console.error("Error de conexión a MongoDB:", error);
+  }
 }
 
 // Llamada inicial para establecer la conexión
 connectToDatabase();
 
 // Rutas
-app.get('/', (req, res) => {
-    res.send('API de tareas');
+app.get("/", (req, res) => {
+  res.send("API de tareas");
 });
 
 // Rutas
-app.use('/auth', authRouter); // Ruta de autenticación
+app.use("/auth", authRouter); // Ruta de autenticación
 
 // Protege las rutas de tareas con JWT
-app.use('/tasks', authenticateJWT, tasksRoute);
+app.use("/tasks", authenticateJWT, tasksRoute);
 
 // Middleware para manejar errores
 app.use((err, req, res, next) => {
-    console.error('Error en el servidor:', err);
-    res.status(500).send('Error interno del servidor');
+  console.error("Error en el servidor:", err);
+  res.status(500).send("Error interno del servidor");
 });
 
 module.exports = app; // Exporta la aplicación para que pueda ser utilizada en server.js
